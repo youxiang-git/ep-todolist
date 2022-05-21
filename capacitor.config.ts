3d3d8022993a0246090e304ed124b45d9f6b5287
Samuel Yee <samuelyee@Samuels-MacBook-Pro.local>
2022-05-21 17:19:10 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movie-gallery-boiler',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
