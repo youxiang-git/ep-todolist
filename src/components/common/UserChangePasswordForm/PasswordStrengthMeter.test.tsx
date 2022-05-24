10d6b4989b52f1925c394a557f3564aab6fd5d57
C Youxiang <68730868+youxiang-git@users.noreply.github.com>
2022-05-24 11:03:14 +0000

    Added test code for PasswordStrengthMeter.test

--- src/components/common/UserChangePasswordForm/PasswordStrengthMeter.test.tsx
+++ src/components/common/UserChangePasswordForm/PasswordStrengthMeter.test.tsx
@@ -1,10 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import {within} from '@testing-library/dom'
 import PasswordStrengthMeter from './PasswordStrengthMeter';
-import { PASSWORD_REQUIREMENTS } from '../../../settings';
+
 
 describe('checkPassword', () => {
-    test('blank password should not render progress bar', () => {});
+    test('blank password should not render progress bar', async () => {
+        const { queryByTestId } = render(<PasswordStrengthMeter password="" />);
+        expect(queryByTestId("component-password-strength-bar")).toBeNull();
+    });
 
-    test('simple password should show weak', () => {});
+    test('simple password should show weak', async () => {
+        const {getByTestId} = render(<PasswordStrengthMeter password="easypw" />);
+        const testElementWeak = getByTestId("component-password-strength-bar");
+        expect(testElementWeak).toHaveTextContent("Weak");
+    });
 
-    test('complex password should strong', () => {});
+    test('complex password should strong', () => {
+        const {getByTestId} = render(<PasswordStrengthMeter password="Str0nKp@55w0rD!4Te5t1nG" />);
+        const testElementStrong = getByTestId("component-password-strength-bar");
+        expect(testElementStrong).toHaveTextContent("Strong");
+    });
 });
